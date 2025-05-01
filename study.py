57b13fa3c3c5c53d537da599133c38c6dfb7ad3b
lucasricardocs <lucasricardo@gmail.com>
2025-05-01 20:19:05 -0300

    Update study.py

--- study.py
+++ study.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from datetime import datetime
+from datetime import datetime, timedelta
 import gspread
 from google.oauth2.service_account import Credentials
 import time
